b91fc39cee32e9fc400808ffa475bd6ec7fe9980
mhgolestan <golestan1369@gmail.com>
2025-06-27 13:54:12 +0200

    new migration

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.ForeignKey('List', default=None, on_delete=models.CASCADE)
+    list = models.ForeignKey('List', default=None, on_delete=models.CASCADE, null=True)
 
 class List(models.Model):
     pass
